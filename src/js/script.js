2df603b3b9c06c08631c1d267b8bbe7c7512671b
awysocka <wysocka.anna88@gmail.com>
2020-01-14 19:19:47 +0100

    Add small fixes

--- src/js/script.js
+++ src/js/script.js
@@ -3,19 +3,23 @@
 const hamburgerIcon = document.getElementById("hamburger");
 const nav = document.getElementById('navigation');
 const navLinks = document.getElementsByClassName("nav__item");
+const navOpen = document.querySelector(".nav__container")
 
 // open and close mobile menu after 'click' on hamburger
 hamburgerIcon.onclick = function () {
   hamburgerIcon.classList.toggle("hamburger--active");
   nav.classList.toggle("nav__menu--active");
+  navOpen.classList.toggle("nav__container--active");
 };
 
 // close mobile menu after 'click' on menu link
 const closeMenu = function () {
   hamburgerIcon.classList.remove("hamburger--active");
   nav.classList.remove("nav__menu--active");
+  navOpen.classList.remove("nav__container--active");
 };
 
 for (let navLink of navLinks) {
   navLink.onclick = closeMenu;
 }
+

6fc1cd81eba3fb61649c74fef135015d2b72fc4d
awysocka <wysocka.anna88@gmail.com>
2020-01-24 21:03:04 +0100

    Fix navigation menu

--- src/js/script.js
+++ src/js/script.js
@@ -3,20 +3,20 @@
 const hamburgerIcon = document.getElementById("hamburger");
 const nav = document.getElementById('navigation');
 const navLinks = document.getElementsByClassName("nav__item");
-const navOpen = document.querySelector(".nav__container")
+const navOpen = document.querySelector(".nav__overlay")
 
 // open and close mobile menu after 'click' on hamburger
 hamburgerIcon.onclick = function () {
   hamburgerIcon.classList.toggle("hamburger--active");
-  nav.classList.toggle("nav__menu--active");
-  navOpen.classList.toggle("nav__container--active");
+  nav.classList.toggle("nav--active");
+  navOpen.classList.toggle("nav__overlay--active");
 };
 
 // close mobile menu after 'click' on menu link
 const closeMenu = function () {
   hamburgerIcon.classList.remove("hamburger--active");
-  nav.classList.remove("nav__menu--active");
-  navOpen.classList.remove("nav__container--active");
+  nav.classList.remove("nav--active");
+  navOpen.classList.remove("nav__overlay--active");
 };
 
 for (let navLink of navLinks) {
